bc833220cb1f8bb79f9ee83966dda7c4fc7e8f8e
Holoduned <94117859+Holoduned@users.noreply.github.com>
2023-03-04 13:00:45 +0300

    Add DogAPI Part1

---
+++ public/inf.js
@@ -0,0 +1,7 @@
+function DisplayPhoto() {
+  console.log("gdg");
+}
+
+function DisplayInfo() {
+  console.log("grjg");
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var requestOptions = {
+  method: "GET",
+  headers: myHeaders,
+  redirect: "follow",
+};
+
+async function fetchDogsBreeds() {
+  const response = await fetch(
+    "https://api.thedogapi.com/v1/breeds?page=0",
+    requestOptions
+  );
+  const data = await response.json();
+  createBreedList(data);
+}
+async function fetchDogById(id) {
+  const response = await fetch(
+    "https://api.thedogapi.com/v1/breeds/" + id,
+    requestOptions
+  );
+  return await response.json();
+}
+function createBreedList(breeds) {
+  var breeds_count = Object.keys(breeds).length;
+  const select = document.querySelector(".breed_select");
+  for (let i = 0; i < breeds_count; i++) {
+    let newOption = new Option(breeds[i].name, breeds[i].id);
+    select.appendChild(newOption);
+  }
+  select.onchange = display_photo;
+}
+
+var requestOptionsImage = {
+  method: "GET",
+  redirect: "follow",
+};
+
+async function SaveDogId(value) {
+  // var select = document.querySelector(".breed_select");
+  // var value = select.value;
+  localStorage.setItem("dogId", value);
+}
+async function display_photo() {
+  var select = document.querySelector(".breed_select");
+  var value = select.value;
+  await SaveDogId(value);
+  const dog = await fetchDogById(value);
+
+  const response = await fetch(
+    "https://api.thedogapi.com/v1/images/" + dog.reference_image_id,
+    requestOptionsImage
+  );
+  const data = await response.json();
+  const image = document.querySelector("img");
+  image.setAttribute("src", data.url);
+}
+
+fetchDogsBreeds();
 reportWebVitals();
